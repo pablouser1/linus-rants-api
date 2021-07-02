07555a74413450cad5d66667db86bd8543466db5
Pablo Ferreiro <pferreiromero@gmail.com>
2021-07-02 19:44:30 +0200

    CORS quick fix

--- index.php
+++ index.php
@@ -4,6 +4,15 @@ require __DIR__ . '/vendor/autoload.php';
 require 'actions.php';
 require 'helpers.php';
 
+// CORS //
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+if($_SERVER['REQUEST_METHOD'] === "OPTIONS") {
+	exit;
+}
+
 $rants = new Rants();
 
 // Create Router instance
